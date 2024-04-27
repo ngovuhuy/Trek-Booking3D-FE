e6acd10df38c8a7e7d5779d0e8a704ef3aa880fe
Tienvan2112 <55881057+LYVANTIEN@users.noreply.github.com>
2024-04-27 17:38:05 +0700

    new

--- src/app/components/Homepage.tsx
+++ src/app/components/Homepage.tsx
@@ -33,43 +33,6 @@ const Homepage = () => {
             </li>
           </ul>
         </nav>
-<<<<<<< HEAD
-        <div className="background-img " >
-              <div className="text-bg  text-center pt-60" >
-                <h1 className='text-white  text-4xl  pb-3 font-bold' >WELCOME TO TREK BOOKING</h1>
-                <p className='text-white  text-2xl pb-3'>Lorem ipsum dolor sit amet consectetur. Eget pellentesque congue eget amet vel <br></br> quam molestie bibendum.</p>
-                <div className="intro flex justify-center pb-3">
-                  <div className="seure flex">
-                    <img className='' src="/public/image/check.png" alt="" />
-                    <p className='text-white font-bold ml-2 mb-1 font1rem'>Secure payment</p>
-                  </div>
-                  <div className="seure flex mx-3">
-                    <img className='' src="line.png" alt="" />
-                  </div>
-                  <div className="seure flex">
-                    <img className='' src="clock.png" alt="" />
-                    <p className='text-white font-bold ml-2 mb-1  font1rem'>Quick support</p>
-                  </div>
-                </div>
-                <div className="   justify-center input-search">
-                  <div className="input-map flex justify-center mb-2">
-                  <img className='imgmap mg-16' src="/image/map.png" alt="" />
-                  <input className='input-first' type="text" placeholder='City, destination or hotel...' />
-                  </div>
-                  <div className="input-map1 flex justify-center mb-2">
-                  <img className='imgmap' src="/image/calendar.png" alt="" />
-                  <input className='input-first-checkin' type="text" placeholder='Check-in-out Dates' />
-                  </div>
-                  <div className="input-map1 flex justify-center mb-2">
-                  <img className='imgmap' src="/image/userplus.png" alt="" />
-<input className='input-first-checkin' type="text" placeholder='Guests and Rooms' />
-                  
-                  </div>
-                  <div className="img-search cursor-pointer flex justify-center">
-                     <img src="/image/search1.png" alt="" />
-                  </div>
-                </div>
-=======
         <div className="background-img ">
           <div className="text-bg  text-center pt-60">
             <h1 className="text-white  text-4xl  pb-3 font-bold">
@@ -85,7 +48,6 @@ const Homepage = () => {
                 <p className="text-white font-bold ml-2 mb-1 font1rem">
                   Secure payment
                 </p>
->>>>>>> origin/minhdev
               </div>
               <div className="seure flex mx-3">
                 <img className="" src="line.png" alt="" />
