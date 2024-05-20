5b35be7dfa95dd38029d1cff279fa9de34b1275f
MinhHoSF24 <minhhonhat.sf.work@gmail.com>
2024-05-20 14:13:01 +0700

    minhho add new code
    minhho add new code

--- src/app/components/Homepage.tsx
+++ src/app/components/Homepage.tsx
@@ -326,52 +326,7 @@ const Homepage = () => {
 
           </div>
         </div>
-      <form action="">
-        <div
-          className=" w-full bg-auto bg-no-repeat bg-center"
-          style={{ background: "url(/image/question.png)",backgroundSize: "cover", height: "376px" }}
-        >
-          <div className="container text-center">
-            <div className="pt-5">
-              <p className="font-bold text-2xl text-white">
-                DO YOU HAVE ANY QUESTION ?
-              </p>
-            </div>
-            <div className="mt-4">
-              <input
-                className="h-10 rounded-xl placeholder-gray-500 border"
-                style={{ width: "410px" }}
-                placeholder="Name"
-                type="text"
-              />
-            </div>
-            <div className="mt-4">
-              <input
-                className="h-10 rounded-xl placeholder-gray-500 border"
-                style={{ width: "410px" }}
-                placeholder="Email"
-                type="text"
-              />
-            </div>
-            <div className="mt-4">
-              <input
-                className="h-10 rounded-xl placeholder-gray-500 border"
-                style={{ width: "410px" }}
-                placeholder="Message"
-                type="text"
-              />
-            </div>
-            <div className="mt-3">
-              <button
-                type="submit"
-                className="w-28 h-10 rounded-xl text-white text-base cursor-pointer bg-sky-500 hover:bg-sky-700"
-              >
-                Submit
-              </button>
-            </div>
-          </div>
-        </div>
-      </form>
+      
     
 
 
--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -2,6 +2,51 @@ import Image from 'next/image'
 const Footer = () => {
   return (
     <>
+    {/* ------------------------------------------------------------------------------------------ */}
+
+    <form action="">
+        <div
+          className=" w-full bg-auto bg-no-repeat bg-center"
+          style={{ background: "url(/image/question.png)",backgroundSize: "cover", height: "376px" }}
+        >
+          <div className="container text-center">
+            <div className="pt-5">
+              <p className="font-bold text-2xl text-white">
+                DO YOU HAVE ANY QUESTION ?
+              </p>
+            </div>
+            <div className="mt-4">
+              <input
+                className="h-10 pr-52 pl-3 rounded-xl placeholder-gray-500 border"
+                placeholder="Name"
+                type="text"
+              />
+            </div>
+            <div className="mt-4">
+              <input
+                className="h-10 pr-52 pl-3 rounded-xl placeholder-gray-500 border"
+                placeholder="Email"
+                type="text"
+              />
+            </div>
+            <div className="mt-4">
+              <input
+                className="h-10 pr-52 pl-3 rounded-xl placeholder-gray-500 border"
+                placeholder="Message"
+                type="text"
+              />
+            </div>
+            <div className="mt-3 ml-24">
+              <button
+                type="submit"
+                className="w-28 h-10 rounded-xl text-white text-base cursor-pointer bg-sky-500 hover:bg-sky-700"
+              >
+                Submit
+              </button>
+            </div>
+          </div>
+        </div>
+      </form>
       <footer className="bg-neutral-800">
         <div className="container grid content-center h-full py-8">
           <div className="w-full row">
