d57fee88a01c41e735f8342986371806a1108421
HuyGod <115614497+HuyGod@users.noreply.github.com>
2024-05-24 23:16:43 +0700

    fe cope

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import Homepage from "./components/Homepage";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
   <div>
+       <Navbar></Navbar> 
         <Homepage></Homepage>
         
   </div>
