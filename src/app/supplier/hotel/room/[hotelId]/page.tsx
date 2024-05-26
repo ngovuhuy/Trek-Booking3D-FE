b47fad70b794431cd7aaf86995c3227a48602b81
Nguyen Huu Tai <tainhce150819@fpt.edu.vn>
2024-05-26 14:17:44 +0700

    new

--- src/app/supplier/hotel/room/[hotelId]/page.tsx
+++ src/app/supplier/hotel/room/[hotelId]/page.tsx
@@ -1,19 +1,19 @@
 /* eslint-disable @next/next/no-img-element */
 // pages/room/[hotelId].tsx
-'use client'
-import React, { useEffect, useState } from 'react';
-import roomService from '@/app/services/roomService';
-import Link from 'next/link';
+"use client";
+import React, { useEffect, useState } from "react";
+import roomService from "@/app/services/roomService";
+import Link from "next/link";
 
 const ListRoom = ({ params }: { params: { hotelId: string } }) => {
-
   const [listRoom, setRooms] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     if (params.hotelId) {
-      roomService.getRoomsByHotelId(Number(params.hotelId))
+      roomService
+        .getRoomsByHotelId(Number(params.hotelId))
         .then((data: any) => {
           setRooms(data);
           setLoading(false);
@@ -37,7 +37,11 @@ const ListRoom = ({ params }: { params: { hotelId: string } }) => {
     <div className="relative">
       <div className="search-add ">
         <div className="search-hotel flex">
-          <input type="text" placeholder="Search........." className="input-hotel pl-3" />
+          <input
+            type="text"
+            placeholder="Search........."
+            className="input-hotel pl-3"
+          />
           <img src="/image/search.png" alt="" />
         </div>
         <button className="ml-8 button-add ml-4rem">+ Add room</button>
@@ -50,61 +54,111 @@ const ListRoom = ({ params }: { params: { hotelId: string } }) => {
                 <table className="min-w-full text-start text-sm font-light text-surface dark:text-white border-solid">
                   <thead className="border-b border-neutral-200 font-medium dark:border-white/10 bk-top-table">
                     <tr>
-                      <th scope="col" className="px-6 py-4">RoomId</th>
-                      <th scope="col" className="px-6 py-4 text-center">Name</th>
-                      <th scope="col" className="px-6 py-4">Status</th>
-                      <th scope="col" className="px-6 py-4">View Detail</th>
-                      <th scope="col" className="px-6 py-4">Manage Service</th>
-                      <th scope="col" className="px-6 py-4">Manage Room 3D</th>
-                      <th scope="col" className="px-6 py-4">Manage Room Image</th>
-                      <th scope="col" className="px-6 py-4">Action</th>
+                      <th scope="col" className="px-6 py-4">
+                        RoomId
+                      </th>
+                      <th scope="col" className="px-6 py-4 text-center">
+                        Name
+                      </th>
+                      <th scope="col" className="px-6 py-4">
+                        Status
+                      </th>
+                      <th scope="col" className="px-6 py-4">
+                        View Detail
+                      </th>
+                      <th scope="col" className="px-6 py-4">
+                        Manage Service
+                      </th>
+                      <th scope="col" className="px-6 py-4">
+                        Manage Room 3D
+                      </th>
+                      <th scope="col" className="px-6 py-4">
+                        Manage Room Image
+                      </th>
+                      <th scope="col" className="px-6 py-4">
+                        Action
+                      </th>
                     </tr>
                   </thead>
                   <tbody>
                     {listRoom.length > 0 ? (
                       listRoom.map((item: IRoom, index) => (
-                        <tr key={index} className="border-b border-neutral-200 dark:border-white/10">
-                          <td className="whitespace-nowrap px-6 py-4 font-medium">{item.roomId}</td>
-                          <td className="whitespace-nowrap px-6 py-4 font-semibold">{item.roomName}</td>
-                          <td className={`whitespace-nowrap px-6 py-4 ${item.roomStatus ? "color-active" : "color-stop"}`}>
+                        <tr
+                          key={index}
+                          className="border-b border-neutral-200 dark:border-white/10"
+                        >
+                          <td className="whitespace-nowrap px-6 py-4 font-medium">
+                            {item.roomId}
+                          </td>
+                          <td className="whitespace-nowrap px-6 py-4 font-semibold">
+                            {item.roomName}
+                          </td>
+                          <td
+                            className={`whitespace-nowrap px-6 py-4 ${
+                              item.roomStatus ? "color-active" : "color-stop"
+                            }`}
+                          >
                             {item.roomStatus ? "Active" : "Stopped"}
                           </td>
                           <td className="whitespace-nowrap px-6 py-4">
                             <Link href="#">
-                              <img src="/image/viewdetail.png" alt="View Detail" />
+                              <img
+                                src="/image/viewdetail.png"
+                                alt="View Detail"
+                              />
                             </Link>
                           </td>
                           <td className="whitespace-nowrap px-6 py-4">
                             <Link href="#">
-                              <img src="/image/managevoucher.png" alt="Manage Service" />
+                              <img
+                                src="/image/managevoucher.png"
+                                alt="Manage Service"
+                              />
                             </Link>
                           </td>
                           <td className="whitespace-nowrap px-6 py-4">
                             <Link href="#">
-                              <img src="/image/managevoucher.png" alt="Manage Room 3D" />
+                              <img
+                                src="/image/managevoucher.png"
+                                alt="Manage Room 3D"
+                              />
                             </Link>
                           </td>
                           <td className="whitespace-nowrap px-6 py-4">
                             <Link href="#">
-                              <img src="/image/managevoucher.png" alt="Manage Room Image" />
+                              <img
+                                src="/image/managevoucher.png"
+                                alt="Manage Room Image"
+                              />
                             </Link>
                           </td>
                           <td className="whitespace-nowrap px-6 py-4 flex">
                             <Link href="#">
-                              <img className="w-5 h-5 cursor-pointer" src="/image/pen.png" alt="Edit" />
+                              <img
+                                className="w-5 h-5 cursor-pointer"
+                                src="/image/pen.png"
+                                alt="Edit"
+                              />
                             </Link>
                             <img
                               className="w-5 h-5 cursor-pointer ml-3"
                               src="/image/unlock.png"
                               alt="Delete"
-                              onClick={() => console.log(`Delete room ${item.roomId}`)}
+                              onClick={() =>
+                                console.log(`Delete room ${item.roomId}`)
+                              }
                             />
                           </td>
                         </tr>
                       ))
                     ) : (
                       <tr>
-                        <td colSpan={8} className="text-center py-4 text-red-600 font-bold">No rooms found</td>
+                        <td
+                          colSpan={8}
+                          className="text-center py-4 text-red-600 font-bold"
+                        >
+                          No rooms found
+                        </td>
                       </tr>
                     )}
                   </tbody>
