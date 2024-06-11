9a3ba536e1e780b33451c1553063273f9ee95ec3
HuyGod <115614497+HuyGod@users.noreply.github.com>
2024-06-11 13:49:20 +0700

    merge all

--- src/app/supplier/hotel/room/[hotelId]/page.tsx
+++ src/app/supplier/hotel/room/[hotelId]/page.tsx
@@ -117,30 +117,14 @@ const ListRoom = ({ params }: { params: { hotelId: string } }) => {
   return (
     <div className="relative">
       <div className="search-add">
-      {hotel && (
-          <div className="breadcrumb">
-            <Link
-              href="/supplier/hotel"
-              style={{ color: "black", fontSize: "18px" }}
-            >
-              Hotel
-            </Link>
-            <span
-              style={{
-                color: "black",
-                fontSize: "18px",
-                marginLeft: "5px",
-                marginRight: "5px",
-              }}
-            >
-              {" > "}
-            </span>
-            <span style={{ color: "blue", fontSize: "18px" }}>
-              {hotel.hotelName}
-            </span>
-          </div>
-        )}
+     
         <div className="search-hotel flex">
+        {hotel && (
+
+<span  className="fix-name">
+     Hotel {" > "} <span     style={{ color: "#0cc560", fontSize: "18px" }}>{hotel.hotelName}</span>
+</span>
+)}
           <input
             type="text"
             placeholder="Search........."
@@ -225,7 +209,7 @@ const ListRoom = ({ params }: { params: { hotelId: string } }) => {
                             </Link>
                           </td>
                           <td className="whitespace-nowrap px-6 py-4">
-                            <Link className='flex justify-center' href="#">
+                            <Link className='flex justify-center' href={`/supplier/hotel/room/${params.hotelId}/serviceOfRoom/${item.roomId}`}>
                               <img
                                 src="/image/managevoucher.png"
                                 alt="Manage Service"
