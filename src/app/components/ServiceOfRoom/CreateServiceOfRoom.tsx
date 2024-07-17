6db4de8f8be7a3f32f39d591dcdca150b82e51c2
Tienvan2112 <55881057+LYVANTIEN@users.noreply.github.com>
2024-07-17 15:25:14 +0700

    new service

--- src/app/components/ServiceOfRoom/CreateServiceOfRoom.tsx
+++ src/app/components/ServiceOfRoom/CreateServiceOfRoom.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { toast } from "react-toastify";
-import { Button, Form, Modal } from "react-bootstrap";
+import { Button, Form, Modal, Image, Row, Col } from "react-bootstrap";
 import serviceOfRoom from "@/app/services/serviceOfRoom";
 import useSWR from "swr";
 
@@ -11,6 +11,7 @@ interface Iprops {
   setShowServiceOfRoomCreate: (value: boolean) => void;
   onCreate: () => void;
   thisRoomId: number;
+  existingServices: IService[];
 }
 
 interface IService {
@@ -21,34 +22,60 @@ interface IService {
 }
 
 function CreateServiceOfRoom(props: Iprops) {
-  const { showServiceOfRoomCreate, setShowServiceOfRoomCreate, onCreate, thisRoomId } = props;
-  const [serviceId, setServiceId] = useState<number>(0);
+  const { showServiceOfRoomCreate, setShowServiceOfRoomCreate, onCreate, thisRoomId, existingServices = [] } = props;
+  const [selectedServices, setSelectedServices] = useState<number[]>([]);
 
   const {
     data: listService,
     error,
     mutate: mutateListService,
   } = useSWR("listService", () => serviceOfRoom.getServices());
+  
+//lấy list service hiện có
+  useEffect(() => {
+    if (existingServices) {
+      const existingServiceIds = existingServices.map(service => service.serviceId);
+      setSelectedServices(existingServiceIds);
+    }
+  }, [existingServices]);
 
   const handleSubmit = async () => {
     try {
-      const roomService = {
-        roomId: thisRoomId,
-        serviceId,
-      };
-      const response = await serviceOfRoom.createRoomService(roomService);
+      for (const serviceId of selectedServices) {
+        if (!existingServices.some(service => service.serviceId === serviceId)) {
+          const roomService = {
+            roomId: thisRoomId,
+            serviceId,
+          };
+          await serviceOfRoom.createRoomService(roomService);
+        }
+      }
       toast.success("Add Service Success");
       handleCloseModal();
       onCreate();
     } catch (error) {
-      toast.error("Service have exists!");
+      toast.error("Service already exists!");
       console.error("Error adding service:", error);
     }
   };
 
   const handleCloseModal = () => {
-    setServiceId(0);
+    setSelectedServices([]);
     setShowServiceOfRoomCreate(false);
+    if (existingServices) {
+      const existingServiceIds = existingServices.map(service => service.serviceId);
+      setSelectedServices(existingServiceIds);
+    }
+  };
+
+  const handleSelectService = (serviceId: number) => {
+    if (!existingServices.some(service => service.serviceId === serviceId)) {
+      setSelectedServices((prevSelectedServices) =>
+        prevSelectedServices.includes(serviceId)
+          ? prevSelectedServices.filter((id) => id !== serviceId)
+          : [...prevSelectedServices, serviceId]
+      );
+    }
   };
 
   return (
@@ -67,19 +94,24 @@ function CreateServiceOfRoom(props: Iprops) {
         <Modal.Body>
           <Form>
             <Form.Group className="mb-3" controlId="formService">
-              <Form.Label>Service</Form.Label>
-              <Form.Control
-                as="select"
-                value={serviceId}
-                onChange={(e: any) => setServiceId(Number(e.target.value))}
-              >
-                <option value={0}>Please select a service</option>
-                {listService && listService.map((service: IService) => (
-                  <option key={service.serviceId} value={service.serviceId}>
-                    {service.serviceName}
-                  </option>
-                ))}
-              </Form.Control>
+              <Form.Label>Services</Form.Label>
+              <Row>
+                {listService && listService.map((service: IService) => {
+                  const isExistingService = existingServices.some(existingService => existingService.serviceId === service.serviceId);
+                  return (
+                    <Col key={service.serviceId} xs={6} className="d-flex align-items-center mb-3">
+                      <Image src={service.serviceImage} thumbnail className="me-2" style={{ width: "30px", height: "35px" }} />
+                      <Form.Check
+                        type="checkbox"
+                        label={service.serviceName}
+                        checked={selectedServices.includes(service.serviceId)}
+                        onChange={() => handleSelectService(service.serviceId)}
+                        disabled={isExistingService}
+                      />
+                    </Col>
+                  );
+                })}
+              </Row>
             </Form.Group>
           </Form>
         </Modal.Body>
